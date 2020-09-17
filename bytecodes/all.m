0da05c8aea4572bbb31cbc50a2552dff854bd4a6
gorgyboy <jlrogb@gmail.com>
2020-09-17 20:40:58 +0000

    Ad bytecode folder with test files

---
+++ bytecodes/all.m
@@ -0,0 +1,63 @@
+push 0
+push 1
+push 2
+  push 3
+                   pall    
+push 4
+    push 5    
+      push    6        
+pall
+nop
+push 0 Push 0 onto the stack
+push 1 Push 1 onto the stack
+
+push 2
+  push 3
+                   pall    
+
+
+                           
+push 4
+
+    push 5    
+      push    6        
+
+nop
+push 1
+push 2
+push 3
+pall
+nop
+push 1
+pint
+push 2
+pint
+push 3
+pint
+nop
+push 1
+push 2
+push 3
+pall
+pop
+pall
+pop
+pall
+pop
+pall
+nop
+push 1
+push 2
+push 3
+pall
+swap
+pall
+nop
+push 1
+push 2
+push 3
+pall
+add
+pall
+
+nop
