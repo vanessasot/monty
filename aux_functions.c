5f59d0cd022e54bbf95584888d4da6e92c977537
gorgyboy <jlrogb@gmail.com>
2020-09-17 19:20:12 -0500

    add is_int function

--- aux_functions.c
+++ aux_functions.c
@@ -43,3 +43,35 @@ void free_stack(stack_t *top)
 		free(temp);
 	}
 }
+
+/**
+ * is_int - check if the string is integer
+ * @str: string to check
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int is_int(char *str)
+{
+	char *alpha = {'0', '1', '2', '3', '4',
+			'5', '6', '7', '8', '9'};
+
+	size_t i_alpha, i_str;
+	int is_alpha;
+
+	for (i_str = 0; i_str < strlen(str); i_str++)
+	{
+		is_alpha = 0;
+		for (i_alpha = 0; i_alpha < 10; i_alpha++)
+		{
+			if (str[i_str] == alpha[i_alpha] || str[0] == '-')
+			{
+				is_alpha = 1;
+				break;
+			}
+		}
+		if (is_alpha == 0)
+			return (0);
+	}
+	return (1);
+}
