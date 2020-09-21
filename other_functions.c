bee0c99fe79b6fe710cc794db5bace3b789f2830
Vanessa <vannexxaa@gmail.com>
2020-09-21 04:12:06 +0000

    Update mod function

--- other_functions.c
+++ other_functions.c
@@ -127,6 +127,11 @@ void opcode_mod(stack_t **top, unsigned int line_num)
 	(void) *top;
 	(void) line_num;
 
+	if ((*top)->n == 0)
+	{
+		dprintf(STDERR_FILENO, "L%d: division by zero\n", line_num);
+		free_stack(args.top);
+	}
 	if (*top && (*top)->prev)
 	{
 		(*top)->prev->n %= (*top)->n;
